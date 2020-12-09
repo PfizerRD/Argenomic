339d03361bfaf61d5cb335bbed71e934a76bf46a
Mohammad Rezaei <mohammadali.rezaei@pfizer.com>
2020-12-09 11:09:23 -0500

    Fixed single-rule case for arbiter

--- argenomic/infrastructure.py
+++ argenomic/infrastructure.py
@@ -109,7 +109,9 @@ class arbiter:
     """
     def __init__(self) -> None:
         self.rules_dict = pd.read_csv(os.path.join(config['root_dir'], "data/smarts/alert_collection.csv"))
-        self.rules_dict = self.rules_dict[self.rules_dict.rule_set_name.isin(config['arbiter']['rules'])]
+        picked_rules = list(config['arbiter']['rules']) if len(config['arbiter']['rules']) == 1 \
+                                                         else config['arbiter']['rules']
+        self.rules_dict = self.rules_dict[self.rules_dict.rule_set_name.isin(picked_rules)]
         self.rules_list = self.rules_dict["smarts"].values.tolist()
         self.tolerance_list = pd.to_numeric(self.rules_dict["max"]).values.tolist()
         self.pattern_list = [Chem.MolFromSmarts(smarts) for smarts in self.rules_list]
